450a44a1736cb10eab49e70af43b5e3dcb1971d3
Voodoobear <s.chalain@gmail.com>
2018-10-12 17:54:06 +0200

    Intégration du formulaire de contact php

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(function() {
+
+    $('.navbar a, footer a').on('click', function(event) {
+        event.preventDefault();
+        let hash = this.hash;
+
+        $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+        
+    });
+
+    $('#contact-form').submit(function(e) {
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+        
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                 
+                if(json.isSuccess) 
+                {
+                    $('#contact-form').append("<p class='thank-you'>Votre message a bien été envoyé.</p>");
+                    $('#contact-form')[0].reset();
+                }
+                else
+                {
+                    $('#firstname + .comments').html(json.firstnameError);
+                    $('#name + .comments').html(json.nameError);
+                    $('#email + .comments').html(json.emailError);
+                    $('#phone + .comments').html(json.phoneError);
+                    $('#message + .comments').html(json.messageError);
+                }                
+            }
+        });
+    });
+})
